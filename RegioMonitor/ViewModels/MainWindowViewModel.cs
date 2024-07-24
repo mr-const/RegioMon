129d65fe85ff174a248cfc6d590f2d64f46789d6
Konst Kolesnichenko <konst@enestech.net>
2024-07-24 14:24:14 +0200

    * added monitoring mode

--- RegioMonitor/ViewModels/MainWindowViewModel.cs
+++ RegioMonitor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Threading;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +13,8 @@ namespace RegioMon.ViewModels;
 
 public partial class MainWindowViewModel : ObservableObject
 {
+    const int RequestTimeout = 5000;
+
     [ObservableProperty]
     public DateTimeOffset _departureDate;
 
@@ -30,12 +33,13 @@ public partial class MainWindowViewModel : ObservableObject
     private readonly ILogger<MainWindowViewModel> _logger;
     private readonly RegioJetApi _rjApi;
 
-    private CancellationTokenSource? _cancelInstallTokenSource = new();
+    private CancellationTokenSource? _cancelMonitoringTokenSource = new();
 
     [ObservableProperty]
     public bool _isSearchRunning = false;
 
-    public IRelayCommand RequestTrainListCommand { get; init; }
+    public IRelayCommand BeginMonitoringCommand { get; init; }
+    public IRelayCommand CancelMonitoringCommand { get; init; }
 
     public MainWindowViewModel(
         ILogger<MainWindowViewModel> logger,
@@ -46,32 +50,62 @@ public partial class MainWindowViewModel : ObservableObject
         _logger = logger;
         _rjApi = rjApi;
 
-        RequestTrainListCommand = new RelayCommand(RequestTrainList);
+        BeginMonitoringCommand = new RelayCommand(StartMonitoring);
+        CancelMonitoringCommand = new RelayCommand(StopMonitoring);
+
         DepartureDate = new DateTime(2024, 8, 7);
     }
 
-    private async void RequestTrainList()
+    private async Task RequestTrainList(CancellationToken ct)
     {
+        IsSearchRunning = true;
         try
         {
-            var resp = await _rjApi.SimpleRouteSearch(DepartureDate.ToString("yyyy-MM-dd"), 5990055004, 10202003);
-            if (resp != null)
+            while (!ct.IsCancellationRequested)
             {
-                _logger.LogInformation("Retrieved {Count} trains", resp.Routes.Length);
-                ErrorMessage = string.Empty;
-
-                Trains.Clear();
-                foreach (var trip in resp.Routes)
+                try
                 {
-                    trip.SetIsRequestedFound(trip.DepartureTime.Date == DepartureDate.Date && trip.Bookable);
-                    Trains.Add(trip);
+                    var resp = await _rjApi.SimpleRouteSearch(DepartureDate.ToString("yyyy-MM-dd"), 5990055004, 10202003);
+                    if (resp != null)
+                    {
+                        _logger.LogInformation("Retrieved {Count} trains", resp.Routes.Length);
+                        ErrorMessage = string.Empty;
+
+                        Trains.Clear();
+                        foreach (var trip in resp.Routes)
+                        {
+                            trip.SetIsRequestedFound(trip.DepartureTime.Date == DepartureDate.Date && trip.Bookable);
+                            Trains.Add(trip);
+                        }
+                    }
                 }
+                catch (Exception ex)
+                {
+                    ErrorMessage = ex.Message;
+                    _logger.LogError(ex, "Failed to retrieve trains: {Message}", ex.Message);
+                }
+
+                ct.WaitHandle.WaitOne(RequestTimeout);
             }
         }
-        catch (Exception ex)
+        finally
         {
-            ErrorMessage = ex.Message;
-            _logger.LogError(ex, "Failed to retrieve trains: {Message}", ex.Message);
+            IsSearchRunning = false;
+            _logger.LogInformation("Monitoring task stopped");
         }
+    }
+
+    private void StopMonitoring()
+    {
+        _cancelMonitoringTokenSource?.Cancel();
+        _logger.LogInformation("Monitoring stopped");
+    }
+
+    public void StartMonitoring()
+    {
+        _logger.LogInformation("Monitoring started");
+        _cancelMonitoringTokenSource = new CancellationTokenSource();
+
+        Task.Run(() => RequestTrainList(_cancelMonitoringTokenSource.Token));
     }
 }
